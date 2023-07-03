cd779cad1fb682f20fcbb7cd69dc92e2bedcda95
Siddharth Preetham <siddharthpreetham09@gmail.com>
2023-07-03 17:40:58 -0400

    added editable chips

--- main.js
+++ main.js
@@ -1,9 +1,94 @@
 window.onload = onLoad;
 
+//function to build chips
+function create_chip(chip_body){
+  return `<div class="rounded-full bg-blue-200 text-sm flex my-auto max-auto h-8 w-20  mx-2">
+  <span class="mx-auto my-auto">
+       ${chip_body}
+  </span>
+  <span class="mx-auto my-auto closebtn" onclick="this.parentElement.style.display='none'">&times;</span>
+</div> `;
+}
 
+function search_bar(){
+  const search_input = document.getElementById('search-input');
+  const new_value = search_input.value;
+
+
+
+  //delete the 
+  search_input.value = "";
+
+  const chips = document.getElementById('chips');
+  chips.innerHTML+=(create_chip(new_value))
+
+
+  //add chips
+
+//   let input = document.querySelector(".chip-input");
+//   let chips = document.querySelector(".chips");
+  
+//   document.querySelector(".form-field")
+//      .addEventListener('click',() => {
+//         input.style.display = 'block';
+//         input.focus();
+//      });
+     
+//   input.addEventListener('blur',()=>{
+//     input.style.display = 'none';
+//   });
+  
+//   input.addEventListener('keypress', function(event){
+//      if(event.which === 13)
+//      {
+       
+        
+  
+//         chips.appendChild(function ()
+//         {
+//            let _chip = document.createElement('div');
+  
+//            _chip.classList.add('chip');
+//            _chip.addEventListener('click', chipClickHandler);
+  
+//            _chip.append(
+//               (function ()
+//               {
+//                  let _chip_text = document.createElement('span');
+//                  _chip_text.classList.add('chip--text');
+//                  _chip_text.innerHTML = input.value;
+  
+//                  return _chip_text;
+//               })(),
+//               (function ()
+//               {
+//                  let _chip_button = document.createElement('span');
+//                  _chip_button.classList.add('chip--button');
+//                  _chip_button.innerHTML = 'x';
+  
+//                  return _chip_button;
+//               })()
+//            );
+  
+//            return _chip;
+//         }());
+//         input.value = '';
+//      }
+//   });
+    
+//   function chipClickHandler(event){
+//      chips.removeChild(event.currentTarget);
+// }
+
+}
 
 function onLoad(){
-    var map = L.map('map').setView([40.749908945558815,  14.50079038639771], 15);
+
+    //create an onclicklistenser for the search form
+    document.getElementById('search-form').addEventListener('submit', search_bar);
+
+    //leafet
+    let map = L.map('map').setView([40.749908945558815,  14.50079038639771], 15);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
